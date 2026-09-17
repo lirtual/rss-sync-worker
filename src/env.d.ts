caea5dab208becc6c0024bfd65145bb0c72daee4
lirtual <lirtual@outlook.com>
2026-09-17 08:36:31 -0700

    feat: add queue binding types

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,5 +1,12 @@
+interface RefreshMessage {
+  feedId: number;
+  dispatchToken: string;
+  dispatchedAt: number;
+}
+
 interface Env {
   DB: D1Database;
+  REFRESH_QUEUE: Queue<RefreshMessage>;
   READER_USERNAME: string;
   READER_TOKEN: string;
   ADMIN_TOKEN: string;
